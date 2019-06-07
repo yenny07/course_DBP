d34be830a21678da0651e31e9e2da7499c75ea9a
l100320503 <l10032503@gmail.com>
2019-06-07 22:15:04 +0900

    deletecourse

---
+++ WebContent/SQL/deleteCourse.sql
@@ -0,0 +1,62 @@
+CREATE OR REPLACE PROCEDURE deleteCourse(profID IN VARCHAR2, courseID IN VARCHAR2,
+               courseIDNO IN NUMBER)
+IS
+   CURSOR courseLIST(student_id VARCHAR2) IS
+   SELECT NVL(c_id, 0) c_id
+   FROM enroll
+   WHERE s_id = student_id;
+
+
+   courseMAX NUMBER;
+   coursecurrent NUMBER;
+   nYEAR NUMBER;
+   nSEMESTER NUMBER;
+   courseCREDIT NUMBER;
+   
+BEGIN
+   
+   nYEAR := Date2EnrollYear(SYSDATE);
+   nSEMESTER := Date2EnrollSemester(SYSDATE);
+ 
+   
+ /*
+  select c_current, c_credit
+into coursecurrent, courseCREDIT
+from course
+where c_id = courseID AND c_number = courseIDNO AND c_year = nYEAR AND c_semester = nSEMESTER;
+
+UPDATE COURSE
+SET c_current = coursecurrent -1
+where c_id = courseID AND c_number = courseIDNO AND c_year = nYEAR AND c_semester = nSEMESTER;
+
+
+
+DELETE FROM ENROLL
+where c_id = courseID AND c_number = courseIDNO AND c_year = nYEAR AND c_semester = nSEMESTER;
+
+  */
+select c_current, c_credit
+into coursecurrent, courseCREDIT
+from course
+where c_id = courseID AND c_number = courseIDNO AND c_year = nYEAR AND c_semester = nSEMESTER;
+
+UPDATE COURSE
+SET c_current = coursecurrent -1
+where c_id = courseID AND c_number = courseIDNO AND c_year = nYEAR AND c_semester = nSEMESTER;
+
+DELETE FROM ENROLL
+where c_id = courseID AND c_number = courseIDNO AND c_year = nYEAR AND c_semester = nSEMESTER;
+
+DELETE FROM COURSE
+where c_id = courseID AND c_number = courseIDNO AND c_year = nYEAR AND c_semester = nSEMESTER;
+
+
+   
+COMMIT;
+ 
+
+
+END;
+/
+
+ show error;
