66470b3df055244b80b439094414ed8389a964c2
l100320503 <l10032503@gmail.com>
2019-06-07 12:18:54 +0900

    change prof pwd

--- WebContent/SQL/change_pwd_prof.sql
+++ WebContent/SQL/change_pwd_prof.sql
@@ -1,11 +1,11 @@
-CREATE OR REPLACE PROCEDURE change_pwd(
+CREATE OR REPLACE PROCEDURE change_pwd_prof(
 enterId IN VARCHAR2,
 enterPwd IN VARCHAR2
 )
 IS
 BEGIN
- UPDATE student
- SET s_pwd = enterPwd
- WHERE s_id = enterId;
+ UPDATE professor
+ SET p_pwd = enterPwd
+ WHERE p_id = enterId;
 END;
 /
